28d8eb25b914af5145c2320c5c618debd90d11c8
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2024-01-03 18:00:34 +0100

    update CheckOutPage.java with WebElements

--- src/main/java/pages/CheckOutPage.java
+++ src/main/java/pages/CheckOutPage.java
@@ -1,4 +1,28 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CheckOutPage {
+    static String CHECKOUT_PAGE_URL = "https://www.saucedemo.com/checkout-step-one.html";
+
+    public CheckOutPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);}
+
+    @FindBy(id = "first-name")
+    private WebElement firstNameField;
+
+    @FindBy(id = "last-name")
+    private WebElement lastNameField;
+
+    @FindBy(id = "postal-code")
+    private WebElement postalCodeField;
+
+    @FindBy(id = "continue")
+    private WebElement continueButton;
+
+    @FindBy(id = "cancel")
+    private WebElement cancelButton;
 }
