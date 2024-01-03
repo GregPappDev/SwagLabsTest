0e4e532273c069a7a3ae9722c4a8abaea0027937
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-01-03 13:24:18 +0100

    update login service

--- src/main/java/services/LogInService.java
+++ src/main/java/services/LogInService.java
@@ -1,4 +1,27 @@
 package services;
 
+import actions.LoginActions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LogInService {
+    private LoginActions loginActions;
+
+    public LogInService(WebDriver driver, WebDriverWait wait){this.loginActions = new LoginActions(driver,wait);}
+
+    public void performLogin(String username, String password){
+        loginActions.enterUsername(username);
+        loginActions.enterPassword(password);
+        loginActions.clickLoginButton();
+    }
+
+    public void performLoginWithoutCredentials(){loginActions.clickLoginButton();}
+
+    public void performLoginWithoutUsername(String password){
+        loginActions.enterPassword(password);
+        loginActions.clickLoginButton();}
+
+    public void performLoginWithoutPassword(String username){
+        loginActions.enterUsername(username);
+        loginActions.clickLoginButton();}
 }
