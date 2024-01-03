5ede6725cb6061f942f6c1e4376cd97fe7622334
GregPappDev <gregpappdev@gmail.com>
2024-01-03 17:27:13 +0100

    feat: add preliminary 'add-item-to-cart' test to CartTest.java

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,12 +1,15 @@
+import dev.failsafe.internal.util.Assert;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import services.BrowseService;
+import services.CartService;
 
 import java.time.Duration;
 
@@ -14,6 +17,7 @@ public class CartTest {
 
     private WebDriver driver;
     private BrowseService browseService;
+    private CartService cartService;
     private WebDriverWait wait;
     private static  Logger logger = LogManager.getLogger(CartTest.class);
 
@@ -23,12 +27,16 @@ public class CartTest {
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         browseService = new BrowseService(driver, wait, logger);
+        cartService = new CartService(driver, wait, logger);
     }
 
     @Test
     public void AddOneItemToCartSuccessfully(){
         driver.get("https://www.saucedemo.com/");
         browseService.AddFirstItemToCart("standard_user", "secret_sauce");
+        driver.get("https://www.saucedemo.com/cart.html");
+        int numberOfItemsInCart = cartService.numberOfItemsInCart();
+        Assertions.assertEquals(1, numberOfItemsInCart);
     }
 
 
