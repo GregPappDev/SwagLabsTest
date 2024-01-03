c47d6eaa8ee1fe37d89f6fbe184ed354abf8e38a
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-01-03 18:04:51 +0100

    update logout service

--- src/main/java/services/LogOutService.java
+++ src/main/java/services/LogOutService.java
@@ -1,4 +1,17 @@
 package services;
 
+import actions.LogoutActions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LogOutService {
+    private LogoutActions logoutActions;
+    public LogOutService(WebDriver driver, WebDriverWait wait){
+        this.logoutActions = new LogoutActions(driver, wait);
+    }
+
+    public void performLogout(){
+        logoutActions.clickMenuButton();
+        logoutActions.clickLogoutButton();
+    }
 }
