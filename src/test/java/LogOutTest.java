283e5623bb48474345c6b696033698659e624fab
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-01-03 18:53:21 +0100

    update logout test

--- src/test/java/LogOutTest.java
+++ src/test/java/LogOutTest.java
@@ -1,2 +1,27 @@
-public class LogOutTest {
+import baseTests.BaseTest;
+import com.aventstack.extentreports.Status;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import testCases.LogoutTestCases;
+
+public class LogOutTest extends BaseTest {
+
+    private LogoutTestCases logoutTestCases;
+
+    @BeforeEach
+    public void initComponents(){
+        this.logoutTestCases = new LogoutTestCases(driver, wait);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testData/loginData.csv", numLinesToSkip = 1)
+    public void successfulLogout(String username, String password){
+        logger.info("Perform logout with logged in user.");
+        test.log(Status.INFO,"Perform logout with logged in user.");
+
+        boolean successfulLogout = logoutTestCases.performLogout(username, password);
+        Assertions.assertTrue(successfulLogout, "Logout was not successful");
+    }
 }
