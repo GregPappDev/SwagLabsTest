2f7fa690b01ca1a31229bb7f497f4861880ab3ea
GregPappDev <gregpappdev@gmail.com>
2024-01-03 17:01:04 +0100

    feat: set up CartTest.java

--- src/main/java/services/BrowseService.java
+++ src/main/java/services/BrowseService.java
@@ -1,12 +1,13 @@
 package services;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.BrowsePage;
 
 import java.util.List;
-import java.util.logging.Logger;
+
 
 public class BrowseService {
 
@@ -44,4 +45,6 @@ public class BrowseService {
         }
         return counter;
     }
+
+
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,2 +1,38 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import services.BrowseService;
+
+import java.time.Duration;
+
 public class CartTest {
+
+    private WebDriver driver;
+    private BrowseService browseService;
+    private WebDriverWait wait;
+    private static  Logger logger = LogManager.getLogger(CartTest.class);
+
+    @BeforeEach
+    public void init(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        browseService = new BrowseService(driver, wait, logger);
+    }
+
+    @Test
+    public void AddOneItemToCartSuccessfully(){
+        browseService.AddFirstItemToCart();
+    }
+
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
 }
